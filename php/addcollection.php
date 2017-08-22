e9e749ba11ea754b6066439705c2c108ce837d93
zmxn-PC\zmxn <kwor@163.com>
2017-08-22 13:48:59 +0800

    收场收藏判断

--- php/addcollection.php
+++ php/addcollection.php
@@ -4,6 +4,14 @@ require_once("dbconn.php");
  
 	$shopid=$_REQUEST["shopid"]?$_REQUEST["shopid"]:0;
 	 
+	 
+	$sql1="select count(*) as num from kshop_collection where uid='$uid' and shopid='$shopid'";
+	$res1=mysqli_query($dbconn,$sql1);
+	$row1 = mysqli_fetch_array($res1);
+	 if($row1["num"]>0){
+	 	echo "已经收藏过了";
+		
+	 }else{
 	$sql = "insert into kshop_collection (uid,shopid,addtime) VALUES('$uid','$shopid',now())";
 	 
 	$res=mysqli_query($dbconn,$sql);
@@ -12,4 +20,5 @@ require_once("dbconn.php");
 	}else{
 		echo "error";
 	}
+	 }
 ?>
